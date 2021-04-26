93f9dc7639533f1400bf94ae0930d27231d065e6
zayfix <zayyfix@gmail.com>
2021-04-26 16:29:59 +0200

    Fix conneries de Cameron :fire:

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,27 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Grid from "@material-ui/core/Grid";
-import "./index.css";
-import Index from "./routes/route";
-import Camera from "./components/camera";
-import BottomNav from "./components/BottomNav";
-import Container from '@material-ui/core/Container';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Route from './routes/route';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Grid container spacing={1}>
-      <Grid container item xs={12} spacing={3}>
-        <Index />
-      </Grid>
-      <Grid container item xs={12} spacing={3}>
-        <Camera />
-      </Grid>
-      <Grid container item xs={12} spacing={3}>
-        <Container>
-          <BottomNav />
-        </Container>
-      </Grid>
-    </Grid>
+    <Route />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
