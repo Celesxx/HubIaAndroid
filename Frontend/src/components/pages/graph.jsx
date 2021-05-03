d666448f716f4dcdef0aefb2f20c15c38d8cf513
zayfix <zayyfix@gmail.com>
2021-05-03 16:02:09 +0200

    Grid for graph and export piechart

--- Frontend/src/components/pages/graph.jsx
+++ Frontend/src/components/pages/graph.jsx
@@ -2,85 +2,79 @@ import React from "react";
 import "../css/core.css";
 import "../css/input.css";
 import Paper from "@material-ui/core/Paper";
-import { PieChart, Pie, Cell } from "recharts";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 
-const data = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
-];
+import PieChart from "./components/pieChart";
 
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+const useStyles = makeStyles((theme) => ({
+  bigpaper: {
+    margin: 50,
+    backgroundColor: "#252C32",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    margin: 10,
+    backgroundColor: "#383F47",
+  },
+}));
 
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-  index,
-}: any) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+export default function FullWidthGrid() {
+  const classes = useStyles();
 
   return (
-    <text
-      x={x}
-      y={y}
-      fill="white"
-      textAnchor={x > cx ? "start" : "end"}
-      dominantBaseline="central"
-    >
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
+    <header className="body">
+      <Paper className={classes.bigpaper} elevation={3}>
+        <Grid container spacing={1}>
+          <Grid item xs={4}>
+            <Paper className={classes.paper} elevation={3}>
+              <PieChart></PieChart>
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper} elevation={3}>
+              xs=4
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper} elevation={3}>
+              xs=4
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper} elevation={3}>
+              xs=4
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper} elevation={3}>
+              xs=4
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper} elevation={3}>
+              xs=4
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper} elevation={3}>
+              xs=4
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper} elevation={3}>
+              xs=4
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper} elevation={3}>
+              xs=4
+            </Paper>
+          </Grid>
+        </Grid>
+      </Paper>
+    </header>
   );
-};
-class graph extends React.Component {
-  render() {
-    return (
-      <header className="body">
-        <Paper
-          elevation={3}
-          style={{
-            margin: 50,
-            backgroundColor: "#252C32",
-          }}
-        >
-          <Paper
-            elevation={3}
-            style={{
-              margin: 10,
-              backgroundColor: "#383F47",
-            }}
-          >
-            <PieChart width={400} height={400}>
-              <Pie
-                data={data}
-                cx={200}
-                cy={200}
-                labelLine={false}
-                label={renderCustomizedLabel}
-                outerRadius={80}
-                fill="#8884d8"
-                dataKey="value"
-              >
-                {data.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORS[index % COLORS.length]}
-                  />
-                ))}
-              </Pie>
-            </PieChart>
-          </Paper>
-        </Paper>
-      </header>
-    );
-  }
 }
-
-export default graph;
